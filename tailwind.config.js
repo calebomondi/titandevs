39b9ee18dc9ce5b158d8e83e26aa5cb2aece1330
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2024-12-16 21:10:41 +0300

    updated require daisy ui to import daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from "daisyui"
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,6 +9,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
